804facd2f5df9a67ec0b3bff1cdeacf83b553426
estefany89 <esilrob582@g.educaand.es>
2024-11-07 13:09:01 +0100

    Añadir ejercicio

--- Tema5/ejercicio2.js
+++
@@ -1,8 +0,0 @@
-//2. Crea un array booleano con dos valores 'verdadero' y 'falso'. Este array te sirve para
-//traducir los valores true y false. Usalo para que el script muestre un alert con el nombre
-//vedadero si un usuario teclea un valor entre 0 y 9 y falso en el caso contrario.
-
-var booleanos = ["falso", "verdadero"];
-var numero = prompt("Introduce un número entre 0 y 9:");
-var esValido = (numero >= 0 && numero <= 9);
-alert(booleanos[esValido ? 1 : 0]);
